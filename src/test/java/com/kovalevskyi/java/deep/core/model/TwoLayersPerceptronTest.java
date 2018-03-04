969bd000f46fca95e868ab2e76451dbd735826d3
Viacheslav Kovalevskyi <vkovalevskyi@google.com>
2018-03-04 15:25:30 -0800

    Amount of epochs increased in the 2 layers persiptron test in order to make it less flaky.

--- src/test/java/com/kovalevskyi/java/deep/core/model/TwoLayersPerceptronTest.java
+++ src/test/java/com/kovalevskyi/java/deep/core/model/TwoLayersPerceptronTest.java
@@ -60,7 +60,7 @@ public class TwoLayersPerceptronTest {
     @Test
     public void testTraining() throws Exception {
         double error = .0;
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 1200; i++) {
             error =
                     trainIteration(
                             1.,
