8ba40445da630e0534202e0a3c3a9d4d6c0bf43b
Andrey K <kotkovets.a.a@gmail.com>
2018-03-31 23:54:09 +0300

    Change Map to Linear Algebra matrix

--- src/main/java/com/dj/core/model/graph/ConnectedNeuron.java
+++ src/main/java/com/dj/core/model/graph/ConnectedNeuron.java
@@ -2,18 +2,29 @@ package com.dj.core.model.graph;
 
 import com.dj.core.model.activation.ActivationFunction;
 import com.google.common.util.concurrent.AtomicDouble;
+import org.apache.commons.math3.linear.*;
 
 import java.util.*;
 
 public class ConnectedNeuron implements Neuron {
+
+    private final static int VECTOR_ROWS_COUNT = 1;
+    private final static int VECTOR_ROW_INDEX = 0;
 
     private final Context context;
 
     private final ActivationFunction activationFunction;
 
     /**
-     * Represents the connections from the neuron to the neurons that it receives signals from. For example in the
-     * following network:
+     * Represents a relation between a neuron and a neuron index (it's position). It's necessary to store pairs
+     * (Neuron, Index) to have a relation between neurons and their weights (in the {@link #backwardConnections}) and
+     * their input signals (in the {@link #inputSignals}).
+     */
+    private final Map<Neuron, Integer> neuronIndexes = new HashMap<>();
+
+    /**
+     * Represents the connections (their weights) from the neuron to the neurons that it receives signals from.
+     * For example in the following network:
      * NeuronA ___
      *            \ weight1 = -0.1
      *             \
@@ -23,12 +34,10 @@ public class ConnectedNeuron implements Neuron {
      *             /
      *            /  weight3 = 0.8
      * NeuronC ---
-     * backwardConnections map will looks like this:
-     * NeuronA => -0.1
-     * NeuronB => 0.1
-     * NeuronC => 0.8
+     *
+     * backwardConnections tensor will looks like this: [ -0.1, 0.1, 0.8 ]
      */
-    private final Map<Neuron, Double> backwardConnections = new HashMap<>();
+    private RealMatrix backwardConnections = new Array2DRowRealMatrix();
 
     /**
      * Represents set of the Neurons to which current neuron sends signals to. There is no need in weights here.
@@ -38,11 +47,12 @@ public class ConnectedNeuron implements Neuron {
     private final String name;
 
     /**
-     * inputSignals is used to store the signals from other Neurons. Keys in this Map should be absolutely identical to
-     * the keys in the {@link #backwardConnections}. As soon as all the signals received Neuron can start processing
-     * them.
+     * inputSignals is used to store the signals from other Neurons. Values in this tensor should be stored according to
+     * {@link #neuronIndexes}. If the neuron NeuronX was added to the {@link #neuronIndexes} with the index 0, the
+     * {@link #inputSignals} will store the NeuronX's input signal at the position (rowIndex, 0).
+     * As soon as all the signals received Neuron can start processing them.
      */
-    private final Map<Neuron, Double> inputSignals = new HashMap<>();
+    private RealMatrix inputSignals = new Array2DRowRealMatrix();
 
     private final AtomicDouble bias;
 
@@ -58,7 +68,7 @@ public class ConnectedNeuron implements Neuron {
      * the way of getting this value.
      */
     private volatile double forwardResult;
-    
+
     private volatile double inputSignalsSum;
 
     private volatile double inputSignalsAverage;
@@ -84,11 +94,11 @@ public class ConnectedNeuron implements Neuron {
     @Override
     public void forwardSignalReceived(final Neuron from, final Double value) {
         signalReceived++;
-        inputSignals.put(from, value);
+        inputSignals.setEntry(VECTOR_ROW_INDEX, neuronIndex(from), value);
         inputSignalsSum += value;
         // The following if is the check weather current signal was the last remaining signal to receive. And if so and
         // all incoming signals have been received the Neuron can start processing them and issue new signal himself.
-        if (backwardConnections.size() == signalReceived) {
+        if (backwardConnections.getColumnDimension() == signalReceived) {
             // 4 steps need to happen when Neuron processes the input signals:
             // 1. Calculate input = W * X + b
             // 2. Calculate output = f(input), where f is activation function
@@ -99,15 +109,8 @@ public class ConnectedNeuron implements Neuron {
             // Calculating W * X + b - sum of all input signals, each signal multiplied on the corresponding weight.
             // Bias is added at the end.
             forwardInputToActivationFunction
-                    = backwardConnections
-                        .entrySet()
-                        .stream()
-                        .mapToDouble(connection ->
-                                // inputSignals store the actual signal, while connection.getValue() gives you the
-                                // weight that the signal should be multiplied to. Therefore this part is X * W.
-                                inputSignals.get(connection.getKey())
-                                        * connection.getValue())
-                        .sum() + bias.get();
+                    = backwardConnections.multiply(inputSignals.transpose()).getEntry(0, 0) + bias.get();
+
             if (context.isDebugMode()) {
                 if (brokenValue(forwardInputToActivationFunction)) {
                     throw new RuntimeException("Forward input to activation function is broken");
@@ -124,10 +127,10 @@ public class ConnectedNeuron implements Neuron {
             forwardConnections
                     .stream()
                     .forEach(connection ->
-                        connection
-                                .forwardSignalReceived(
-                                        ConnectedNeuron.this,
-                                        signalToSend)
+                            connection
+                                    .forwardSignalReceived(
+                                            ConnectedNeuron.this,
+                                            signalToSend)
                     );
 
             // Step #4
@@ -163,24 +166,18 @@ public class ConnectedNeuron implements Neuron {
         if (derivative == 0.) {
             return;
         }
-        backwardConnections.keySet().forEach(conn ->
-            backwardConnections.compute(conn, (k, weight) -> {
-                    final double newWeight = weight + inputSignals.get(conn) * dz * context.getLearningRate();
-                    if (context.isDebugMode()) {
-                        if (brokenValue(newWeight)) {
-                            throw new RuntimeException("Updated connection weight is broken");
-                        }
-                    }
-                    return newWeight;
-                }
-            ));
+
+        double dzLearningRate = dz * context.getLearningRate();
+        backwardConnections = backwardConnections.add(inputSignals.scalarMultiply(dzLearningRate));
+
         bias.addAndGet(inputSignalsAverage * dz * context.getLearningRate());
-        backwardConnections
-                .keySet()
+        neuronIndexes
+                .entrySet()
                 .stream()
-                .forEach(conn ->
-                    conn.backwardSignalReceived(backwardConnections.get(conn) * dz)
-                );
+                .forEach(neuronIndex ->
+                        neuronIndex.getKey().backwardSignalReceived(
+                                backwardConnections
+                                        .getEntry(VECTOR_ROW_INDEX, neuronIndex.getValue()) * dz));
     }
 
     @Override
@@ -190,8 +187,20 @@ public class ConnectedNeuron implements Neuron {
 
     @Override
     public void addBackwardConnection(final Neuron neuron, final Double weight) {
-        backwardConnections.put(neuron, weight);
-        inputSignals.put(neuron, Double.NaN);
+        int nextIndex = neuronIndexes.size();
+        neuronIndexes.put(neuron, nextIndex);
+
+        backwardConnections = addToTensor(backwardConnections, nextIndex, weight);
+        inputSignals = addToTensor(inputSignals, nextIndex, Double.NaN);
+    }
+
+    private RealMatrix addToTensor(RealMatrix tensor, int newColumnIndex, double value) {
+        RealMatrix oldTensor = tensor;
+        tensor = new Array2DRowRealMatrix(VECTOR_ROWS_COUNT, newColumnIndex + 1);
+        if (oldTensor.getColumnDimension() != 0)
+            tensor.setSubMatrix(oldTensor.getData(), 0, 0);
+        tensor.setEntry(VECTOR_ROW_INDEX, newColumnIndex, value);
+        return tensor;
     }
 
     @Override
@@ -225,6 +234,10 @@ public class ConnectedNeuron implements Neuron {
         return signalReceived == 0;
     }
 
+    private int neuronIndex(Neuron n) {
+        return neuronIndexes.get(n);
+    }
+
     public static class Builder {
 
         private double bias = new Random().nextDouble();
@@ -254,7 +267,7 @@ public class ConnectedNeuron implements Neuron {
             this.context = context;
             return this;
         }
-        
+
         public ConnectedNeuron build() {
             if (activationFunction == null) {
                 throw new RuntimeException("ActivationFunction need to be set in order to" +
